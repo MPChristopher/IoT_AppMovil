782c94685b45224006451a4ef82ac21a2941c2a1
GOJuanIgnacio <nachobz200607@gmail.com>
2024-02-02 06:19:20 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IoT_AppMovil',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
